15cbff3b386467a3f5170f9b5ecddf1f619db4b2
Abiy Melaku <agirma08@gmail.com>
2016-08-19 13:41:44 -0700

    Added test cases for function repeat

--- spec/algorithms.js
+++ spec/algorithms.js
@@ -27,8 +27,26 @@ describe('factorialize', function () {
   it('should calculate the factorial', () => {
     expect(factorialize(5)).to.equal(120);
     expect(factorialize(10)).to.equal(3628800);
-    expect(factorialize(20)).to.equal(2432902008176640000);  
+    expect(factorialize(20)).to.equal(2432902008176640000);
     expect(factorialize(0)).to.equal(1);
+  });
+
+  // Add more assertions here
+});
+
+
+describe('repeat', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(repeat).to.be.a('function');
+  });
+
+  it('should repeat str count number of times', () => {
+    expect(repeat('*', 3)).to.equal('***');
+    expect(repeat("abc", 3)).to.equal("abcabcabc");
+    expect(repeat("abc", 4)).to.equal("abcabcabcabc");
+    expect(repeat("abc", 1)).to.equal("abc");
   });
 
   // Add more assertions here
