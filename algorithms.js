257de55b633a7a0a83628b4369bc50112de8f379
Abiy Melaku <agirma08@gmail.com>
2016-08-19 13:41:16 -0700

    Added skeleton for function repeat

--- algorithms.js
+++ algorithms.js
@@ -8,4 +8,14 @@ var reverseString = function (str) {
 //Factorialize a Number
 var factorialize = function (num) {
   // your code goes here
-}  
+}
+
+//it should take a string and repeat it a number of times
+var repeat = function(str, count) {
+  // your code goes here
+  var phrase = ''
+  for(var i = 0; i < count; i++){
+    phrase += str;
+  }
+  return phrase;
+};
